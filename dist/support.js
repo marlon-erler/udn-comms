b047f1b5e83524bff13565d13978ba45825122b2
marlon-erler <viridian035@gmail.com>
2024-07-07 16:56:12 +0200

    update

--- dist/support.js
+++ dist/support.js
@@ -1,3 +1,4 @@
+// TABS
 document.querySelector("main").addEventListener("scroll", () => {
   const links = document.querySelectorAll(".tab-link");
   links.forEach((element) => element.removeAttribute("active"));
@@ -8,3 +9,23 @@ document.querySelector("main").addEventListener("scroll", () => {
 
   if (links[currentPage]) links[currentPage].setAttribute("active", "");
 });
+
+// SERVICE WORKER
+async function registerServiceWorker() {
+  if ("serviceWorker" in navigator) {
+    try {
+      const registration = await navigator.serviceWorker.register("/sw.js", {
+        scope: "/",
+      });
+      if (registration.installing) {
+        setTimeout(() => window.location.reload(), 500);
+      } else if (registration.active) {
+        console.log("Service worker active");
+      }
+    } catch (error) {
+      console.error(`Could not install service worker: ${error}`);
+    }
+  }
+}
+
+registerServiceWorker();
