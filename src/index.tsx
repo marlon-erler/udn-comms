5da13eace5e9d8c2ca62904de15232644cbf7d89
marlon-erler <viridian035@gmail.com>
2024-07-19 21:12:16 +0200

    remove maintenance view, set up index

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,12 @@
-import "./View/maintenance";
-
+import ConnectionModel from "./Model/connectionModel";
 import StorageModel from "./Model/storageModel";
 
 const storageModel = new StorageModel();
+const connectionModel = new ConnectionModel({
+    connectionChangeHandler() {
+        
+    },
+    messageHandler(data) {
+        
+    },
+})
