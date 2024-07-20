1cced969f63b5dcf32dcbbdda3467f3af68db228
marlon-erler <viridian035@gmail.com>
2024-07-20 04:17:10 +0200

    prepare chatViewModel

---
+++ src/ViewModel/chatViewModel.tsx
@@ -0,0 +1,13 @@
+import * as React from "bloatless-react";
+
+import { ChatModel } from "../Model/chatModel";
+import StorageModel from "../Model/storageModel";
+
+export default class ChatViewModel {
+    chatModel: ChatModel;
+
+    // init
+    constructor(storageModel: StorageModel) {
+        
+    }
+}
